5ea5395be617d034c60a3076f81c62a9c35e58fa
fuliping <1349497941@qq.com>
2019-11-21 00:07:37 +0800

    添加模型类Pl

--- tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
@@ -1,14 +1,21 @@
 package com.tensquare.qa.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.tensquare.qa.pojo.Problem;
+import com.tensquare.qa.pojo.Pl;
+import org.springframework.data.jpa.repository.Query;
+
 /**
  * problem数据访问接口
  * @author Administrator
  *
  */
 public interface ProblemDao extends JpaRepository<Problem,String>,JpaSpecificationExecutor<Problem>{
-	
+
+	@Query("select p from  Problem  p where id in (select problemid from Pl where labelid = ?1 ) order by  replytime desc")
+    Page<Problem> findNewListByLabelId(String labelId, Pageable pageable);
 }
---
+++ tensquare_qa/src/main/java/com/tensquare/qa/pojo/Pl.java
@@ -0,0 +1,36 @@
+package com.tensquare.qa.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 〈中间表pl〉
+ *
+ * @author: fuliping
+ * @date: 2019/11/20 11:05 下午
+ */
+@Entity
+@Table(name = "tb_pl")
+public class Pl {
+    @Id
+    private String problemid;
+    @Id
+    private  String lableid;
+
+    public String getProblemid() {
+        return problemid;
+    }
+
+    public void setProblemid(String problemid) {
+        this.problemid = problemid;
+    }
+
+    public String getLableid() {
+        return lableid;
+    }
+
+    public void setLableid(String lableid) {
+        this.lableid = lableid;
+    }
+}

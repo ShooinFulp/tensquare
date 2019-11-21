e88d4961bd3bd37157dbc47ac3b1417edb22ddc1
fuliping <1349497941@qq.com>
2019-11-22 00:13:38 +0800

    完成文章微服务

--- tensquare_article/src/main/java/com/tensquare/article/dao/ArticleDao.java
+++ tensquare_article/src/main/java/com/tensquare/article/dao/ArticleDao.java
@@ -4,11 +4,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.tensquare.article.pojo.Article;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 /**
  * article数据访问接口
  * @author Administrator
  *
  */
 public interface ArticleDao extends JpaRepository<Article,String>,JpaSpecificationExecutor<Article>{
-	
+	@Modifying
+    @Query("update Article  set state = '1'  where  id = ?1")
+     void examine(String id);
+
+    @Modifying
+    @Query("update Article  set thumbup = thumbup+ 1 where  id = ?1")
+    void updateThumbup(String id);
 }
